32e1629f0ea21868e1a2ba5dc34d7ec9f09e34ee
SandraSimjanoska <sandra.simjanoska@live.com>
2025-04-13 21:35:53 +0200

    Add files via upload

---
+++ Class02.Homework/SQL.Class02.Homework.TalentLMS.sql
@@ -0,0 +1,63 @@
+--Find all Students with FirstName = Antonio
+SELECT *
+FROM Student
+WHERE FirstName = 'Antonio';
+
+--Find all Students with DateOfBirth greater than ‘01.01.1999’
+SELECT *
+FROM Student
+WHERE DateOfBirth > '1999-01-01';
+
+--Find all Students with LastName starting With ‘J’ enrolled in January/1998
+SELECT *
+FROM Student
+WHERE LastName LIKE 'J%' 
+
+--List all Students ordered by FirstName
+SELECT *
+FROM Student
+ORDER BY FirstName;
+
+--List all Teacher Last Names and Student Last Names in single result set.
+--Remove duplicates
+SELECT DISTINCT LastName
+FROM (
+    SELECT LastName FROM Student
+    UNION
+    SELECT LastName FROM Teacher
+) AS CombinedLastNames;
+
+--Create Foreign key constraints from diagram or with script
+ALTER TABLE Grade
+ADD CONSTRAINT FK_Grade_Student
+FOREIGN KEY (StudentID) REFERENCES Student(ID);
+
+ALTER TABLE Grade
+ADD CONSTRAINT FK_Grade_Teacher
+FOREIGN KEY (TeacherID) REFERENCES Teacher(ID);
+
+ALTER TABLE Grade
+ADD CONSTRAINT FK_Grade_Course
+FOREIGN KEY (CourseID) REFERENCES Course(ID);
+
+ALTER TABLE GradeDetails
+ADD CONSTRAINT FK_GradeDetails_Grade
+FOREIGN KEY (GradeID) REFERENCES Grade(ID);
+
+ALTER TABLE GradeDetails
+ADD CONSTRAINT FK_GradeDetails_AchievementType
+FOREIGN KEY (AchievementTypeID) REFERENCES AchievementType(ID);
+
+--List all possible combinations of Courses names and AchievementType
+--names that can be passed by student
+SELECT c.Name AS CourseName, a.Name AS AchievementType
+FROM Course c
+CROSS JOIN AchievementType a;
+
+--List all Teachers without exam Grade
+SELECT *
+FROM Teacher
+WHERE ID NOT IN (
+    SELECT DISTINCT TeacherID
+    FROM Grade
+);
---
+++ Class02.Homework/SQL.Class02.Workshops2&3.sql
@@ -0,0 +1,82 @@
+-- WORKSHOP 02
+--Calculate the total amount per BusinessEntity on all orders in the
+--system and filter only total orders greater than 635558
+
+SELECT BusinessEntityId,
+SUM(TotalPrice) AS TotalAmount
+FROM Orders
+GROUP BY BusinessEntityID
+HAVING SUM(TotalPrice) > 635558;
+
+--Calculate the total amount per BusinessEntity on all orders in the
+--system from Customers with ID < 20 and filter only total orders less than 100000
+
+SELECT BusinessEntityId,
+SUM(TotalPrice) AS TotalAmount
+FROM Orders
+WHERE CustomerId < 20
+GROUP BY BusinessEntityId
+HAVING SUM(TotalPrice) < 100000;
+
+--Find the Maximal Order amount, and the Average Order amount
+--per BusinessEntity on all orders in the system. Filter only records
+--where Total order amount is more than 4x bigger than average
+
+SELECT BusinessEntityId,
+MAX(TotalPrice) AS MaxOrderAmount,
+AVG(TotalPrice) AS AverageOrderAmount
+FROM Orders
+GROUP BY BusinessEntityId
+HAVING MAX(TotalPrice) > 4 * AVG(TotalPrice);
+
+
+
+--WORKSHOP 03
+--Create new view (vv_CustomerOrders) that will List all CustomerIds and sum of total Orders per customer
+CREATE VIEW vv_CustomerOrders
+AS
+SELECT CustomerId, SUM(o.TotalPrice) AS TotalOrdersPerCustomer 
+FROM Orders o
+GROUP BY CustomerId
+
+--Change the view to show Customer Names instead of CustomerId
+ALTER VIEW vv_CustomerOrders 
+AS
+SELECT c.Name, SUM(TotalPrice) AS TotalOrdersPerCustomer
+FROM Orders o
+JOIN Customers c ON o.CustomerId = c.Id
+GROUP BY c.Name
+
+--Change the view to show the results ordered by the customer with biggest total price
+ALTER VIEW vv_CustomerOrders 
+AS
+SELECT c.Name, o.TotalPrice 
+FROM Orders o
+JOIN Customers c ON o.CustomerId = c.Id
+GROUP BY c.Name, o.TotalPrice
+SELECT *
+FROM vv_CustomerOrders
+ORDER BY TotalPrice DESC;
+
+--Create new view (vv_EmployeeOrders) that will List all Employees 
+--(FirstName and LastName) , Product name and total quantity sold
+CREATE OR alter VIEW vv_EmployeeOrders AS
+SELECT e.FirstName, e.LastName, p.Name AS ProductName, SUM(od.Quantity) as TotalQuantitySold 
+FROM Employees e
+INNER JOIN Orders o ON e.Id = o.EmployeeId
+INNER JOIN OrderDetails od ON o.Id = od.OrderId
+INNER JOIN Products p ON od.ProductId = p.Id
+GROUP BY e.FirstName, e.LastName, p.Name
+
+--Alter the view to show only sales from Business entities belonging to region 'Skopski'
+CREATE OR ALTER VIEW vv_EmployeeOrders AS
+SELECT e.FirstName, e.LastName, p.Name AS ProductName, SUM(od.Quantity) AS TotalQuantitySold
+FROM Employees e
+INNER JOIN Orders o ON e.Id = o.EmployeeId
+INNER JOIN OrderDetails od ON o.Id = od.OrderId
+INNER JOIN Products p ON od.ProductId = p.Id
+INNER JOIN BusinessEntities be ON o.BusinessEntityId = be.Id
+WHERE be.Region = 'Skopski'
+GROUP BY e.FirstName, e.LastName, p.Name
+
+select * from vv_EmployeeOrders
